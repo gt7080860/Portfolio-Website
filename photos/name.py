386981c7cb6ae57d1df4e99a011c19765f384977
gt780860 <gt7080860@gmail.com>
2025-10-21 17:45:52 +0530

    ✨ Added cinematic landing page with photo slideshow animation

--- photos/name.py
+++
@@ -1,8 +0,0 @@
-import os
-
-folder_path = r"C:\Users\Gaurav Tiwari\OneDrive\Desktop\Portfolio website\photos"
-
-files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-
-for file in files:
-    print(file)
